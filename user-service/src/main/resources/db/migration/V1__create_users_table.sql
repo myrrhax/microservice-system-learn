87b7cacfa80f63812858d73475cc650bcc65041e
Денис Сизинцев <sizintsevdenis05@mail.ru>
2026-05-24 22:54:43 +0300

    create user table migration

---
+++ user-service/src/main/resources/db/migration/V1__create_users_table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE users
+(
+    id                        BIGSERIAL PRIMARY KEY,
+    name                      VARCHAR(100)     NOT NULL,
+    surname                   VARCHAR(100)     NOT NULL,
+    email                     VARCHAR(255)     NOT NULL UNIQUE,
+    address                   TEXT,
+    alerting                  BOOLEAN          NOT NULL DEFAULT FALSE,
+    energy_alerting_threshold DOUBLE PRECISION NOT NULL DEFAULT 0 CHECK ( energy_alerting_threshold >= 0 )
+);
