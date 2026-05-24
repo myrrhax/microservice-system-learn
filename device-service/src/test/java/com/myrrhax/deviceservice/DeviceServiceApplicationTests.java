54d5053affc798fd4067d0b9076cbb62ad0d5a8c
Денис Сизинцев <sizintsevdenis05@mail.ru>
2026-05-25 00:25:43 +0300

    initial device-service

---
+++ device-service/src/test/java/com/myrrhax/deviceservice/DeviceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myrrhax.deviceservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeviceServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
