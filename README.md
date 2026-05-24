3ff92852a37b6714faf611a27490485d9cea9930
Денис Сизинцев <sizintsevdenis05@mail.ru>
2026-05-24 22:11:29 +0300

    docker compose dev setup

---
+++ README.md
@@ -0,0 +1,10 @@
+### run in dev mode
+
+```shell
+docker compose -f docker-compose-dev.yml up -d
+```
+
+### stop dev containers
+```shell
+docker compose -f docker-compose-dev.yml down
+```
--- user-service/build.gradle.kts
+++ user-service/build.gradle.kts
@@ -20,10 +20,11 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-flyway")
     implementation("org.springframework.boot:spring-boot-starter-webmvc")
+    implementation("org.springframework.boot:spring-boot-starter-flyway")
     compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
+
     testImplementation("org.springframework.boot:spring-boot-starter-data-jpa-test")
     testImplementation("org.springframework.boot:spring-boot-starter-flyway-test")
     testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
