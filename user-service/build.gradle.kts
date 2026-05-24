ebac29855888d44e764927dbeb0a779cd007eec3
Денис Сизинцев <sizintsevdenis05@mail.ru>
2026-05-24 22:29:56 +0300

    add postgres dependencies

--- user-service/build.gradle.kts
+++ user-service/build.gradle.kts
@@ -22,6 +22,9 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-webmvc")
     implementation("org.springframework.boot:spring-boot-starter-flyway")
+    implementation("org.flywaydb:flyway-database-postgresql")
+    runtimeOnly("org.postgresql:postgresql")
+
     compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
 
