c7d91b2bd0b56487a0ce7cb225ad64df8cd7232d
Денис Сизинцев <sizintsevdenis05@mail.ru>
2026-05-24 22:59:39 +0300

    user entity and repository

---
+++ user-service/src/main/java/com/myrrhax/userservice/entity/User.java
@@ -0,0 +1,39 @@
+package com.myrrhax.userservice.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String surname;
+
+    private String email;
+
+    private String address;
+
+    private boolean alerting;
+
+    @Column(name = "energy_alerting_threshold")
+    private double energyAlertingThreshold;
+}
---
+++ user-service/src/main/java/com/myrrhax/userservice/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.myrrhax.userservice.repository;
+
+import com.myrrhax.userservice.entity.User;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+}
