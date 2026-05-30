4b8384e4182d4cc17cf5ea8b7f42ec5689ab6642
Денис Сизинцев <sizintsevdenis05@mail.ru>
2026-05-30 21:24:14 +0300

    add gemini call

--- insight-service/src/main/java/com/myrrhax/insightservice/service/InsightService.java
+++ insight-service/src/main/java/com/myrrhax/insightservice/service/InsightService.java
@@ -1,10 +1,14 @@
 package com.myrrhax.insightservice.service;
 
 import com.myrrhax.insightservice.client.UsageClient;
+import com.myrrhax.insightservice.dto.DeviceDto;
 import com.myrrhax.insightservice.dto.InsightDto;
 import com.myrrhax.insightservice.dto.UsageDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +17,7 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class InsightService {
     private final UsageClient usageClient;
+    private final ChatModel chatModel;
 
     @Value("${app.overview-days:3}")
     private int overviewDays;
@@ -22,6 +27,29 @@ public class InsightService {
         UsageDto usageData = usageClient.getXDaysUsageForUser(userId, overviewDays)
                 .orElseThrow();
 
-        return null;
+        double totalUsage = usageData.devices().stream()
+                .mapToDouble(DeviceDto::energyConsumed)
+                .sum();
+
+        log.info("User {} total energy consumption is {}", userId, totalUsage);
+
+        // Call gemini
+        String prompt = new StringBuilder()
+                .append("Analyse the following energy usage data and provide a ")
+                .append("concise overview with actionable insights.")
+                .append("This data is the aggregated data for the past ")
+                .append(overviewDays)
+                .append(" days.")
+                .append("Usage Data: \n")
+                .append(usageData.devices())
+                .toString();
+
+        ChatResponse response = chatModel.call(
+                Prompt.builder()
+                        .content(prompt)
+                        .build()
+        );
+
+        return new InsightDto(userId, response.getResult().getOutput().getText(), totalUsage);
     }
 }
