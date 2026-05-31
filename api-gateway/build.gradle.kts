3b31882f3993e3440f74d2068ec849d65e3bcda2
Денис Сизинцев <sizintsevdenis05@mail.ru>
2026-05-31 23:44:13 +0300

    add reactive api gateway

--- api-gateway/build.gradle.kts
+++ api-gateway/build.gradle.kts
@@ -22,6 +22,7 @@ extra["springCloudVersion"] = "2025.1.1"
 
 dependencies {
     implementation("org.springframework.cloud:spring-cloud-starter-gateway-server-webflux")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.projectreactor:reactor-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
