cfba1ed9484e6332a0971f40927f42ca9b4dff83
Денис Сизинцев <sizintsevdenis05@mail.ru>
2026-06-11 22:39:36 +0300

    add basic keycloak config

--- api-gateway/build.gradle.kts
+++ api-gateway/build.gradle.kts
@@ -26,6 +26,7 @@ dependencies {
     implementation("org.springframework.cloud:spring-cloud-starter-circuitbreaker-reactor-resilience4j")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.projectreactor:reactor-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
---
+++ api-gateway/src/main/java/com/myrrhax/apigateway/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.myrrhax.apigateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityWebFilterChain filterChain(ServerHttpSecurity http) {
+        return http
+                .authorizeExchange(exchange ->
+                        exchange.anyExchange().authenticated())
+                .oauth2ResourceServer(resourceServer ->
+                        resourceServer.jwt(Customizer.withDefaults()))
+                .build();
+    }
+}
