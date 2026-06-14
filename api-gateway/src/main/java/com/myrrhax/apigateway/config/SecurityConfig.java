3d60bf80d13504fbc149670e865881194cd909a9
Денис Сизинцев <sizintsevdenis05@mail.ru>
2026-06-14 15:40:23 +0300

    enable stateless session management

--- api-gateway/src/main/java/com/myrrhax/apigateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/myrrhax/apigateway/config/SecurityConfig.java
@@ -5,12 +5,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.context.NoOpServerSecurityContextRepository;
 
 @Configuration
 public class SecurityConfig {
     @Bean
     public SecurityWebFilterChain filterChain(ServerHttpSecurity http) {
         return http
+                .securityContextRepository(NoOpServerSecurityContextRepository.getInstance())
                 .authorizeExchange(exchange -> exchange.anyExchange().permitAll())
                 .oauth2ResourceServer(config -> config.jwt(Customizer.withDefaults()))
                 .build();
